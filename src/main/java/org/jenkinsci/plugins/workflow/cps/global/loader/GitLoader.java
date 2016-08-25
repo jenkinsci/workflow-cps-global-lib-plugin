3a8f3b42657366ff985ef835d6b4caf1ff3f8cf4
attrobit <info@attrobit.de>
2016-08-25 16:44:36 +0200

    avadoc.

--- src/main/java/org/jenkinsci/plugins/workflow/cps/global/loader/GitLoader.java
+++ src/main/java/org/jenkinsci/plugins/workflow/cps/global/loader/GitLoader.java
@@ -1,26 +1,3 @@
-/*
- * The MIT License
- *
- * Copyright 2016 philipplang.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 package org.jenkinsci.plugins.workflow.cps.global.loader;
 
 import java.io.File;
@@ -33,14 +10,14 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import jenkins.model.Jenkins;
 import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
 /**
+ * Loads the Groovy-Files from a specific git repository.
  *
- * @author philipplang
+ * @author Philipp Lang
  */
 public class GitLoader implements Loader, Parser {
 
@@ -50,7 +27,6 @@ public class GitLoader implements Loader, Parser {
 
     @Override
     public List<Loader> parse(String script) {
-        System.out.println("- parsing script " + script);
         List<Loader> result = new ArrayList<>();
 
         Pattern pattern = Pattern.compile("^//#git.*$", Pattern.MULTILINE);
@@ -58,7 +34,6 @@ public class GitLoader implements Loader, Parser {
         if (matcher.lookingAt()) {
             result.add(GitLoader.fromString(matcher.group(0)));
         }
-        System.out.println("- parsed loaders: " + result.size());
         return result;
     }
 
@@ -67,7 +42,7 @@ public class GitLoader implements Loader, Parser {
         GitLoader loader = new GitLoader();
         loader.repositoryUrl = splitted[1];
         loader.branch = splitted[2];
-        if(splitted.length > 3) {
+        if (splitted.length > 3) {
             loader.clPath = splitted[3];
         }
         return loader;
@@ -85,7 +60,7 @@ public class GitLoader implements Loader, Parser {
                     .setBranch(branch)
                     .setDirectory(targetFile)
                     .call();
-            if(clPath != null) {
+            if (clPath != null) {
                 targetFile = new File(targetFile, clPath);
             }
             return targetFile.toURI().toURL();
--- src/test/java/org/jenkinsci/plugins/workflow/cps/global/loader/GitLoaderTest.java
+++ src/test/java/org/jenkinsci/plugins/workflow/cps/global/loader/GitLoaderTest.java
@@ -1,26 +1,3 @@
-/*
- * The MIT License
- *
- * Copyright 2016 philipplang.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 package org.jenkinsci.plugins.workflow.cps.global.loader;
 
 import java.util.List;
@@ -28,13 +5,17 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- *
- * @author philipplang
+ * Unit test for {@link GitLoader}.
+ * 
+ * @author Philipp Lang
  */
 public class GitLoaderTest {
 
-    private GitLoader instance = new GitLoader();
+    private final GitLoader instance = new GitLoader();
 
+    /**
+     * Test script parsing with one url
+     */
     @Test
     public void testParseOneUrl() {
 
