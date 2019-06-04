7654317b8b28a5c7987da16159c06bcb745de1dc
Devin Nusbaum <d.w.nusbaum@gmail.com>
2019-06-04 15:09:21 -0400

    [JENKINS-56682] Reproduce issue using types from shared libraries in initializers

--- src/test/java/org/jenkinsci/plugins/workflow/libs/LibraryAdderTest.java
+++ src/test/java/org/jenkinsci/plugins/workflow/libs/LibraryAdderTest.java
@@ -53,6 +53,7 @@ import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import static org.junit.Assert.*;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.Rule;
 import org.jvnet.hudson.test.BuildWatcher;
@@ -334,6 +335,24 @@ public class LibraryAdderTest {
         r.assertLogContains("set to loaded BAR", r.buildAndAssertSuccess(p));
         p.setDefinition(new CpsFlowDefinition("@Library('config@master') _; echo(/set to $config.foo/)", true));
         r.assertLogContains("set to loaded null", r.buildAndAssertSuccess(p));
+    }
+
+    @Ignore
+    @Issue("JENKINS-56682")
+    @Test public void scriptVarsWhereInitializerUsesLibrary() throws Exception {
+        sampleRepo.init();
+        sampleRepo.write("src/pkg/Foo.groovy", "package pkg; class Foo { }");
+        sampleRepo.git("add", "src");
+        sampleRepo.git("commit", "--message=init");
+        GlobalLibraries.get().setLibraries(Collections.singletonList(
+            new LibraryConfiguration("lib",
+                new SCMSourceRetriever(new GitSCMSource(null, sampleRepo.toString(), "", "*", "", true)))));
+        WorkflowJob p = r.jenkins.createProject(WorkflowJob.class, "p");
+        p.setDefinition(new CpsFlowDefinition(
+                "@Library('lib@master') import pkg.Foo\n" +
+                "import groovy.transform.Field\n" +
+                "@Field f = new Foo()\n", true));
+        r.buildAndAssertSuccess(p);
     }
 
 }
